74dae9547f52f50b9db0e6f4a4a5cbe1cc68fb0f
shrutichobe09 <shruti.chobe@atyeti.com>
2024-12-05 15:57:27 +0530

    updated java workflow

--- test.java
+++ test.java
@@ -1,5 +1,5 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("Hello, World! from shruti");
+        System.out.println("Hello, World! from shruti chobe ");
     }
 }
